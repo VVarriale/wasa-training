e282add2971ee39ed41899788e15fa8e924a9f16
VVarriale <33053088+VVarriale@users.noreply.github.com>
2025-12-03 18:02:37 +0100

    Create Concurrency3.go

---
+++ Lesson4/Concurrency3.go
@@ -0,0 +1,16 @@
+// Channels
+func main() {
+  var channel = make(chan int)
+  go func() {
+    var i = 0
+    for i < 10 {
+      channel <- i
+      i++
+    }
+  }()
+  var j = 0
+  for j < 10 {
+    fmt.Println(<-channel)
+    j++
+  }
+}
