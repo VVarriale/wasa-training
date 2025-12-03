add1d885c33a25005a5da58872b6671e91f6c477
VVarriale <33053088+VVarriale@users.noreply.github.com>
2025-12-03 23:35:49 +0100

    Update function2.js

--- Lesson6-js/function2.js
+++ Lesson6-js/function2.js
@@ -6,5 +6,5 @@
  var y = makeMultiple(5); // 10
  var f = makeMultiple; // functionmakeMultiple(x){varmult=2;returnmult*x}
  var n = f(100); // 200
- • makeMultiple()referstothefunctioninvocation
- • makeMultiplereferstotheobjectfunction
+ //• makeMultiple()referstothefunctioninvocation
+ //• makeMultiplereferstotheobjectfunction
