b916cb98d511f2229b633adc0658104829860557
VVarriale <33053088+VVarriale@users.noreply.github.com>
2025-12-03 17:00:16 +0100

    Create GoInterfacces1.go

---
+++ Lesson4/GoInterfacces1.go
@@ -0,0 +1,11 @@
+type Vertex struct {
+X int
+Y int
+}
+func (v Vertex) Equal(other Vertex) bool {
+return v.X == other.X && v.Y == other.Y
+}
+// Somewhere in the code
+vtx := Vertex{1, 2}
+if vtx.Equal(Vertex{2, 3}) {
+// ...
