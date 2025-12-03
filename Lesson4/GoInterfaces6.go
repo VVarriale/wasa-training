abc2a5960bbba154f82385a1f52c90172d66779d
VVarriale <33053088+VVarriale@users.noreply.github.com>
2025-12-03 18:24:00 +0100

    Fix formatting of Writer and ResponseWriter interfaces

--- Lesson4/GoInterfaces6.go
+++ Lesson4/GoInterfaces6.go
@@ -1,14 +1,14 @@
 //Writer
 // io/io.go:96
 type Writer interface {
-Write(p []byte) (n int, err error)
+  Write(p []byte) (n int, err error)
 }
 // net/http/server.go:95
 type ResponseWriter interface {
-Header() Header
-Write([]byte) (int, error)
-WriteHeader(statusCode int)
+  Header() Header
+  Write([]byte) (int, error)
+  WriteHeader(statusCode int)
 }
 func saveFile(w io.Writer, content []byte) {
-w.Write(content)
+  w.Write(content)
 }
